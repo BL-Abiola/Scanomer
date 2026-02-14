1a499175cf6701b654720c3bf84cefcd918c5c9f
Abdulganiyu Lukman <lukman4real001@gmail.com>
2026-02-14 04:52:58 +0000

    Link should be directed to https://x.com/BL_Abiola and the email to :Abi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -355,11 +355,11 @@ export default function Home() {
                           <div>
                               <h3 className="font-semibold text-foreground">Contact</h3>
                               <div className="mt-2 grid grid-cols-2 gap-2">
-                                  <a href="https://twitter.com/firebase" target="_blank" rel="noopener noreferrer" className={cn(buttonVariants({ variant: "outline" }))}>
+                                  <a href="https://x.com/BL_Abiola" target="_blank" rel="noopener noreferrer" className={cn(buttonVariants({ variant: "outline" }))}>
                                       <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="mr-2 h-4 w-4 fill-current"><title>X</title><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
                                       X
                                   </a>
-                                  <a href="mailto:support@example.com" className={cn(buttonVariants({ variant: "outline" }))}>
+                                  <a href="mailto:Abiolalabs29@gmail.com" className={cn(buttonVariants({ variant: "outline" }))}>
                                       <Mail className="mr-2 h-4 w-4" />
                                       Email
                                   </a>
